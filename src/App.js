d2cf9a01c3e5cdc7bb40d6b46f1ae35cfc51b86a
TillMac <49164115+TillMac@users.noreply.github.com>
2022-12-27 02:49:35 +0800

    DR-46 feat：新增可顯示真實數據的 hightlight 區塊 (#1)
    * DR-46 feat：新增 Highlight Cars
    
    1. 建立 RWD Highlights 區塊
    2. 使用 useReducer 管理 Cards 的點擊時狀態
    
    * DR-46 fix：修改 reducer 使其更簡潔
    
    * DR-46 feat：Highlight Data 內容已為真實數據
    
    問題：
    1. Highlights 區塊無法顯示真實資料。
    
    原因：
    1. 尚未撰寫相關 functions 來將對應真實資料傳入。
    
    解決方法：
    1. 先修改 <App /> 中的資料，將其以「GRP_ID」為分類方式，ID 相同資料會 insert 至該「GRP_ID」object 的 data array 中。
    2. 撰寫對應 functions，並將 functions 的結果 dispatch 至 reducer 中。
    3. 在資料處理的等待期間，使用 MUI 的 <CircleProgress /> 作為 loading 動畫。
    
    * DR-46 fix：根據 code review 修改對應 code
    
    問題：
    1. 部分 code 重工，過於冗長與吃效能
    
    解決方法：
    1. 修改 groupData 的分類、生成方式，使其只要跑一次 5,400 筆資料；
    2. 將 4 個點擊用的 action type 修改成統一一個 `CLICKED` action type；
    3. 修改 `UPDATE_STATE`。
    
    * DR-46 chore：刪除多餘 code

--- src/App.js
+++ src/App.js
@@ -10,47 +10,72 @@ import Header from './components/Header';
 import Content from './components/Content';
 import NavBar from './components/NavBar';
 import Loading from './components/Loading';
+import Highlights from './components/Highlights';
 
 const theme = createTheme({ ...themeOptions });
 
 const initDates = (data) => {
-  let dates = [];
-  console.log('data', data);
-  data.forEach(ele => {
-    if (dates.find(date => date === moment(ele.EFF_DT).format("YYYY-MM-DD"))) return;
-    dates.push(moment(ele.EFF_DT).format("YYYY-MM-DD"));
-  })
-  console.log('dates', dates)
-  return dates;
-}
+	let dates = [];
+	console.log('data', data);
+	data.forEach((ele) => {
+		if (dates.find((date) => date === moment(ele.EFF_DT).format('YYYY-MM-DD')))
+			return;
+		dates.push(moment(ele.EFF_DT).format('YYYY-MM-DD'));
+	});
+	console.log('dates', dates);
+	return dates;
+};
+
+// groups = {
+//   "A01": [
+//       {"SEQ_NO":"100","WEE_ID":2,"EFF_DT":"2022-07-18T00:00:00.000Z","WRK_HR":11,"GRP_ID":"A01","CMT_MY":5921.95,"FOB_MY":43979,"PRD_QT":85,"EXP_QT":42,"WRK_QQ":330.05499999999995,"EMP_QT":23,"EMP_QQ":35.3,"I_E_QT":2.27,"CMT_EXP":null},
+//       {"SEQ_NO":"100","WEE_ID":2,"EFF_DT":"2022-07-18T00:00:00.000Z","WRK_HR":11,"GRP_ID":"A01","CMT_MY":5921.95,"FOB_MY":43979,"PRD_QT":85,"EXP_QT":42,"WRK_QQ":330.05499999999995,"EMP_QT":23,"EMP_QQ":35.3,"I_E_QT":2.27,"CMT_EXP":null},
+//     ]
+// }
+
+const getGroups = (originData) => {
+	let groups = [];
+	originData.forEach((rowData) => {
+		if (!groups[rowData.GRP_ID]) {
+			groups[rowData.GRP_ID] = [rowData];
+			return;
+		}
+		groups[rowData.GRP_ID].push(rowData);
+	});
+	return groups;
+};
 
 function App() {
-  const [data, setData] = useState({});
-  const [dates, setDates] = useState({});
-  const [loading, setLoading] = useState(true);
+	const [data, setData] = useState({});
+	const [dates, setDates] = useState({});
+	const [loading, setLoading] = useState(true);
+	const [groupData, setGroupData] = useState(null);
 
-  useEffect(() => {
-    //fetching API data
-    setData(originData)
-    setDates(initDates(originData))
-    setLoading(false);
-  }, [])
+	useEffect(() => {
+		//fetching API data
+		setData(originData);
+		setDates(initDates(originData));
+		//categorizing by groups
+		setGroupData(getGroups(originData));
+		setLoading(false);
+	}, []);
 
-  return (
-    <div className="App">
-      <ThemeProvider theme={theme}>
-        {!loading ? (
-          <Container>
-            <Header />
-            <Content originData={data} dates={dates} />
-            <NavBar originData={data} dates={dates} />
-          </Container>
-        ) : (
-          <Loading />
-        )}
-      </ThemeProvider>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<ThemeProvider theme={theme}>
+				{!loading ? (
+					<Container>
+						<Header />
+						<Highlights groupData={groupData} />
+						<Content originData={data} dates={dates} />
+						<NavBar originData={data} dates={dates} />
+					</Container>
+				) : (
+					<Loading />
+				)}
+			</ThemeProvider>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Highlights/index.jsx
@@ -0,0 +1,199 @@
+import { Box, Card, CardActionArea, Typography } from '@mui/material';
+import React, { useEffect, useReducer } from 'react';
+import IconSelector from '../IconSelector';
+import HightlightNum from './HightlightNum';
+
+const initialHighlightData = [
+	{
+		icon: 'group',
+		num: null,
+		text: 'Total groups',
+		id: 't_grps',
+		isClick: true,
+	},
+	{
+		icon: 'assignmentTurnedIn',
+		num: null,
+		text: 'Completed among',
+		id: 'completed_grps',
+		isClick: false,
+	},
+	{
+		icon: 'emojiEvents',
+		num: null,
+		text: 'Best group',
+		id: 'best_grp',
+		isClick: false,
+	},
+	{
+		icon: 'functions',
+		num: null,
+		text: '總生產 %',
+		id: 't_prd',
+		isClick: false,
+	},
+];
+
+const highlightDataReducer = (state, action) => {
+	let stateCopy = [...state];
+	switch (action.type) {
+		case 'CLICKED':
+			stateCopy.forEach(card => {
+				(card.id === action.payload) ? card.isClick = true : card.isClick = false ;
+			});
+			return stateCopy;
+		case 'UPDATE_STATE':
+			action.payload.forEach(cardData => {
+				let { result, id } = cardData;
+				stateCopy.forEach(card => {
+					if (card.id === id) {card.num = result};
+				})
+			})
+			// stateCopy.forEach(card => {
+			// 	if (card.id === id) {card.num = result};
+				// if (card.icon === 'group') {card.num = action.payload.totalGroup};
+				// if (card.icon === 'assignmentTurnedIn') {card.num = action.payload.assignmentTurnedIn};
+				// if (card.icon === 'emojiEvents') {card.num = action.payload.bestGroup};
+				// if (card.icon === 'functions') {card.num = action.payload.totalProductive};
+			// });
+			return stateCopy;
+		default:
+			return;
+	}
+};
+
+const getCompletedCount = (data, groupName) => {
+	let completedCount = 0;
+	groupName.forEach(groupName => {
+		let rowData = data[groupName];
+		let newestData = rowData[rowData.length - 1];
+		if (newestData.PRD_QT / newestData.EXP_QT >= 1) {completedCount ++};
+	});
+	let payload = { result: completedCount, id: 'completed_grps'};
+	return payload;
+};
+
+const getBestGroup = (data, groupName) => {
+	let score = null;
+	let bestGroup;
+	groupName.forEach(groupName => {
+		let rowData = data[groupName];
+		let newestData = rowData[rowData.length - 1];
+		if (newestData.PRD_QT / newestData.EXP_QT >= score) {
+			score = newestData.PRD_QT / newestData.EXP_QT;
+			bestGroup = groupName;
+		}
+	});
+	let payload = { result: bestGroup, id: 'best_grp'};
+	return payload;
+};
+
+const getTotalProductive = (data, groupName) => {
+	let sum_PRD_QT = 0;
+	let sum_EXP_QT = 0;
+	groupName.forEach(groupName => {
+		let rowData = data[groupName];
+		let newestData = rowData[rowData.length - 1];
+		sum_PRD_QT = sum_PRD_QT + newestData.PRD_QT;
+		sum_EXP_QT = sum_EXP_QT + newestData.EXP_QT;
+	});
+	let result = (sum_PRD_QT / sum_EXP_QT * 100).toFixed(2).toString() + '%';
+	let payload = { result, id: 't_prd'};
+	return payload;
+};
+
+const Highlights = ({ groupData }) => {
+	const [state, dispatch] = useReducer(
+		highlightDataReducer,
+		initialHighlightData
+	);
+
+	useEffect(() => {
+		let groupName = Object.keys(groupData);
+		dispatch({type: 'UPDATE_STATE', payload: [
+			{
+				result: groupName.length,
+				id: 't_grps',
+			},
+			getCompletedCount(groupData, groupName),
+			getBestGroup(groupData, groupName),
+			getTotalProductive(groupData, groupName)
+		]});
+		// 	totalGroup: groupName.length,
+		// 	assignmentTurnedIn: getCompletedCount(groupData, groupName),
+		// 	bestGroup: getBestGroup(groupData, groupName),
+		// 	totalProductive: getTotalProductive(groupData, groupName),
+		// }})
+	}, []);
+
+	const clickHandler = (id) => {
+		dispatch({type: 'CLICKED', payload: id});
+	};
+
+	return (
+		<Box
+			sx={{
+				width: '100%',
+				overflowX: 'auto',
+				backgroundColor: 'rgba(0, 0, 0, 0.2)',
+			}}>
+			<Box
+				sx={{
+					display: 'flex',
+					width: '684px',
+					height: '198px',
+					gap: '20px',
+					alignItems: 'center',
+					justifyContent: 'center',
+					overflowY: 'hidden',
+					mx: 'auto',
+				}}>
+				{state.map((card) => {
+					return (
+						<CardActionArea onClick={() => clickHandler(card.id)} key={card.id}>
+							<Card
+								sx={{
+									width: 156,
+									height: 140,
+									borderRadius: '10px',
+									textAlign: 'center',
+									backgroundColor: !card.isClick
+										? (theme) => theme.palette.background.card
+										: (theme) => theme.palette.primary.main,
+									'& .MuiSvgIcon-root': {
+										color: !card.isClick
+											? (theme) => theme.palette.background.default
+											: 'white',
+									},
+								}}>
+								<Box
+									sx={{
+										mt: '16px',
+									}}>
+										<IconSelector id={card.icon} />
+										<HightlightNum card={card} />
+										<Typography
+											variant='caption'
+											sx={{
+												display: 'block',
+												width: '100%',
+												mt: 0,
+												mb: 4,
+												fontSize: '12px',
+												color: !card.isClick
+													? (theme) => theme.palette.background.default
+													: 'white',
+											}}>
+											{card.text}
+										</Typography>
+								</Box>
+							</Card>
+						</CardActionArea>
+					);
+				})}
+			</Box>
+		</Box>
+	);
+};
+
+export default Highlights;
