1a6d6607f920fa2ded4cb0e90a79534889b4ead1
Reggie <75982750+gn01763009@users.noreply.github.com>
2022-12-27 00:45:28 +0800

    DR-44 🧩 Add loading

--- src/App.js
+++ src/App.js
@@ -15,72 +15,78 @@ import Highlights from './components/Highlights';
 const theme = createTheme({ ...themeOptions });
 
 const initDates = (data) => {
-	let dates = [];
-	console.log('data', data);
-	data.forEach((ele) => {
-		if (dates.find((date) => date === moment(ele.EFF_DT).format('YYYY-MM-DD')))
-			return;
-		dates.push(moment(ele.EFF_DT).format('YYYY-MM-DD'));
-	});
-	console.log('dates', dates);
-	return dates;
+  let dates = [];
+  console.log('data', data);
+  data.forEach((ele) => {
+    if (dates.find((date) => date === moment(ele.EFF_DT).format('YYYY-MM-DD')))
+      return;
+    dates.push(moment(ele.EFF_DT).format('YYYY-MM-DD'));
+  });
+  console.log('dates', dates);
+  return dates;
 };
 
 const getGroups = (originData) => {
-	let groups = [];
-	for (let i = 0; i < originData.length; i++) {
-		if (i === 0) {
-			groups.push({
-				GRP_ID: originData[i].GRP_ID,
-				data: [],
-			});
-		} else {
-			if (originData[i - 1].GRP_ID !== originData[i].GRP_ID) {
-				groups.push({
-					GRP_ID: originData[i].GRP_ID,
-					data: [],
-				});
-			}
-		}
-	}
-	groups.forEach((group) => {
-		group.data = originData.filter((data) => data.GRP_ID === group.GRP_ID);
-	});
-	console.log(groups, 'groups');
-	return groups;
+  let groups = [];
+  for (let i = 0; i < originData.length; i++) {
+    if (i === 0) {
+      groups.push({
+        GRP_ID: originData[i].GRP_ID,
+        data: [],
+      });
+    } else {
+      if (originData[i - 1].GRP_ID !== originData[i].GRP_ID) {
+        groups.push({
+          GRP_ID: originData[i].GRP_ID,
+          data: [],
+        });
+      }
+    }
+  }
+  groups.forEach((group) => {
+    group.data = originData.filter((data) => data.GRP_ID === group.GRP_ID);
+  });
+  console.log(groups, 'groups');
+  return groups;
 };
 
 function App() {
-	const [data, setData] = useState({});
-	const [dates, setDates] = useState({});
-	const [loading, setLoading] = useState(true);
-	const [groupData, setGroupData] = useState(null);
+  const [data, setData] = useState({});
+  const [dates, setDates] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [groupData, setGroupData] = useState(null);
 
-	useEffect(() => {
-		//fetching API data
-		setData(originData);
-		setDates(initDates(originData));
-		setLoading(false);
-		//categorizing by groups
-		setGroupData(getGroups(originData));
-	}, []);
+  useEffect(() => {
+    //fetching API data
+    setData(originData);
+    setDates(initDates(originData));
+    setLoading(false);
+    //categorizing by groups
+    setGroupData(getGroups(originData));
+  }, []);
 
-	return (
-		<div className='App'>
-			<ThemeProvider theme={theme}>
-				{!loading ? (
-					<Container>
-						<Header />
-						<Highlights groupData={groupData} />
-						<Content originData={data} dates={dates} />
-						<NavBar originData={data} dates={dates} />
-					</Container>
-				) : (
-					<Loading />
-				)}
-			</ThemeProvider>
-		</div>
-	);
+  return (
+    <div className='App'>
+      <ThemeProvider theme={theme}>
+        <Container
+          sx={{
+            backgroundColor: (theme) => theme.palette.background.default,
+          }}>
+          {!loading ? (
+            <>
+              <Header />
+              <Highlights groupData={groupData} />
+              <Content originData={data} dates={dates} />
+              <NavBar originData={data} dates={dates} />
+            </>
+          ) : (
+            <Loading />
+          )}
+        </Container>
+
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
