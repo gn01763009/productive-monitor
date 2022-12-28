1016658b5e347920616cda92ed0b8c9d4eabfdad
Reggie <75982750+gn01763009@users.noreply.github.com>
2022-12-28 21:50:11 +0800

    DR-52 🧩 Adding Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Typography } from '@mui/material'
 
 const Header = () => {
   return (
-    <div>
-      header
-    </div>
+    <Typography variant="h4" sx={{color: '#AFBDD1', textAlign:'left', p:2}} component="h2" >
+      EAG
+    </Typography>
   )
 }
 
