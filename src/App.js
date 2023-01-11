5ba610ca9b331bb2893d91e541d45aef8b164ab4
TillMac <danny890308@gmail.com>
2023-01-11 14:23:01 +0800

    hotfix：嘗試使用 proxy 解決 http CORS blocking 問題

--- src/App.js
+++ src/App.js
@@ -15,13 +15,13 @@ import Highlights from './components/Highlights';
 const theme = createTheme({ ...themeOptions });
 
 const initDates = (data) => {
-  let dates = [];
-  data.forEach((ele) => {
-    if (dates.find((date) => date === moment(ele.EFF_DT).format('YYYY-MM-DD')))
-      return;
-    dates.push(moment(ele.EFF_DT).format('YYYY-MM-DD'));
-  });
-  return dates;
+	let dates = [];
+	data.forEach((ele) => {
+		if (dates.find((date) => date === moment(ele.EFF_DT).format('YYYY-MM-DD')))
+			return;
+		dates.push(moment(ele.EFF_DT).format('YYYY-MM-DD'));
+	});
+	return dates;
 };
 
 // groups = {
@@ -33,91 +33,110 @@ const initDates = (data) => {
 // }
 
 const getGroups = (fecthData) => {
-  let groups = [];
-  fecthData.forEach(rowData => {
-    const groupData = {
-      ...rowData,
-      "EFF": Math.round((rowData["PRD_QT"] / rowData["EXP_QT"]) * 1000) / 10,
-      "CMT_P": Math.round((rowData["CMT_MY"] / rowData["EMP_QT"] / rowData["WRK_HR"]) * 1000) / 1000,
-      "CMT_G": Math.round((rowData["CMT_MY"] / rowData["WRK_HR"]) * 1000) / 1000,
-    }
-    if (!groups[rowData.GRP_ID]) {
-      groups[rowData.GRP_ID] = [groupData];
-      return;
-    }
-    groups[rowData.GRP_ID].push(groupData);
-  });
-  return groups;
+	let groups = [];
+	fecthData.forEach((rowData) => {
+		const groupData = {
+			...rowData,
+			EFF: Math.round((rowData['PRD_QT'] / rowData['EXP_QT']) * 1000) / 10,
+			CMT_P:
+				Math.round(
+					(rowData['CMT_MY'] / rowData['EMP_QT'] / rowData['WRK_HR']) * 1000
+				) / 1000,
+			CMT_G: Math.round((rowData['CMT_MY'] / rowData['WRK_HR']) * 1000) / 1000,
+		};
+		if (!groups[rowData.GRP_ID]) {
+			groups[rowData.GRP_ID] = [groupData];
+			return;
+		}
+		groups[rowData.GRP_ID].push(groupData);
+	});
+	return groups;
 };
 
 const get10DGroups = (fecthData) => {
-  let groups = [];
-  const total = fecthData.filter((ele) => (ele.GRP_ID === 'A01')).length;
-  let counter = total;
-  fecthData.forEach(rowData => {
-    counter--;
-    if (counter >= 10) return;
-    const groupData = {
-      ...rowData,
-      "EFF": Math.round((rowData["PRD_QT"] / rowData["EXP_QT"]) * 1000) / 10,
-      "CMT_P": Math.round((rowData["CMT_MY"] / rowData["EMP_QT"] / rowData["WRK_HR"]) * 1000) / 1000,
-      "CMT_G": Math.round((rowData["CMT_MY"] / rowData["WRK_HR"]) * 1000) / 1000,
-    }
-    if (!groups[rowData.GRP_ID]) {
-      groups[rowData.GRP_ID] = [groupData];
-      return;
-    }
-    groups[rowData.GRP_ID].push(groupData);
-    if (counter === 0) {
-      return counter = total;
-    }
-  });
-  return groups;
+	let groups = [];
+	const total = fecthData.filter((ele) => ele.GRP_ID === 'A01').length;
+	let counter = total;
+	fecthData.forEach((rowData) => {
+		counter--;
+		if (counter >= 10) return;
+		const groupData = {
+			...rowData,
+			EFF: Math.round((rowData['PRD_QT'] / rowData['EXP_QT']) * 1000) / 10,
+			CMT_P:
+				Math.round(
+					(rowData['CMT_MY'] / rowData['EMP_QT'] / rowData['WRK_HR']) * 1000
+				) / 1000,
+			CMT_G: Math.round((rowData['CMT_MY'] / rowData['WRK_HR']) * 1000) / 1000,
+		};
+		if (!groups[rowData.GRP_ID]) {
+			groups[rowData.GRP_ID] = [groupData];
+			return;
+		}
+		groups[rowData.GRP_ID].push(groupData);
+		if (counter === 0) {
+			return (counter = total);
+		}
+	});
+	return groups;
 };
 
 function App() {
-  const [data, setData] = useState({});
-  const [dates, setDates] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [groupData, setGroupData] = useState(null);
-  const [isMulti, setIsMulti] = useState(true);
-  const [dataType, setDataType] = useState("");
-  const { fecthData, error } = useFetch('http://125.227.134.205/EAG_EFC/EXC.php');
+	const [data, setData] = useState({});
+	const [dates, setDates] = useState({});
+	const [loading, setLoading] = useState(true);
+	const [groupData, setGroupData] = useState(null);
+	const [isMulti, setIsMulti] = useState(true);
+	const [dataType, setDataType] = useState('');
+	// const { fecthData, error } = useFetch('http://125.227.134.205/EAG_EFC/EXC.php');
+	const { fecthData, error } = useFetch('EAG_EFC/EXC.php');
 
-  useEffect(() => {
-    if (!fecthData) return;
-    //fetching API data
-    setData(getGroups(fecthData));
-    setDates(initDates(fecthData));
-    //categorizing by groups
-    setGroupData(get10DGroups(fecthData));
-    return setLoading(false);
-  }, [fecthData]);
+	useEffect(() => {
+		if (!fecthData) return;
+		//fetching API data
+		setData(getGroups(fecthData));
+		setDates(initDates(fecthData));
+		//categorizing by groups
+		setGroupData(get10DGroups(fecthData));
+		return setLoading(false);
+	}, [fecthData]);
 
-  if (error) return "error";
+	if (error) return 'error';
 
-  return (
-    <div className='App'>
-      <ThemeProvider theme={theme}>
-        <Container
-          sx={{
-            backgroundColor: (theme) => theme.palette.background.default,
-            px: 0,
-          }}>
-          {!loading ? (
-            <>
-              <HeaderMemo />
-              <Highlights data={data} />
-              <Content groupData={groupData} dates={dates} isMulti={isMulti} dataType={dataType} />
-              <NavBarMemo groupData={groupData} setGroupData={setGroupData} data={data} setIsMulti={setIsMulti} setDataType={setDataType} dates={dates} />
-            </>
-          ) : (
-            <Loading />
-          )}
-        </Container>
-      </ThemeProvider>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<ThemeProvider theme={theme}>
+				<Container
+					sx={{
+						backgroundColor: (theme) => theme.palette.background.default,
+						px: 0,
+					}}>
+					{!loading ? (
+						<>
+							<HeaderMemo />
+							<Highlights data={data} />
+							<Content
+								groupData={groupData}
+								dates={dates}
+								isMulti={isMulti}
+								dataType={dataType}
+							/>
+							<NavBarMemo
+								groupData={groupData}
+								setGroupData={setGroupData}
+								data={data}
+								setIsMulti={setIsMulti}
+								setDataType={setDataType}
+								dates={dates}
+							/>
+						</>
+					) : (
+						<Loading />
+					)}
+				</Container>
+			</ThemeProvider>
+		</div>
+	);
 }
 
 export default App;
